5258a6f4943a6e7c8df644ec7178187c5bcf4a6f
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-12-17 12:12:48 +0900

    extract diff among frames

--- src/HARIMOTO/scripts/falling-point-estimation.py
+++ src/HARIMOTO/scripts/falling-point-estimation.py
@@ -27,21 +27,45 @@ def hsv_color_filter(img):
     img_mask = img_mask1 + img_mask2
     filtered_img = cv2.bitwise_and(img, img, mask=img_mask)
     return filtered_img
-    
+
+def moving_object_extractor(previmg,nextimg):
+    ##https://qiita.com/otakoma/items/04216c60fa31eae60947 を参考にした．
+    color_diff = cv2.absdiff(nextimg, previmg)
+    gray_diff = cv2.cvtColor(color_diff, cv2.COLOR_BGR2GRAY)
+    return gray_diff
+
+def dilation_image(dilationSize, kernelSize, img):
+    ##https://qiita.com/otakoma/items/04216c60fa31eae60947 を引用した．
+    kernel = np.ones((kernelSize, kernelSize), np.uint8)
+    element = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (2 * dilationSize + 1, 2 * dilationSize + 1), (dilationSize, dilationSize))
+    dilation_img = cv2.dilate(img, kernel, element)
+    return dilation_img
+
+#def find_balls(img):
+#    image, contours, hierarchy = cv2.findountours(
+#
+
 def main():
     global image_raw
     image_raw = None
+    img = None
     rospy.init_node("falling_estimation", anonymous = True)
     #rospy.Subscriber("/camera/color/image_raw", Image, image_cb)
     rospy.Subscriber("/usb_cam/image_raw", Image, image_cb)
     rate = rospy.Rate(90)
     while not rospy.is_shutdown():
         if image_raw is not None:
+            previmg = img
             img = hsv_color_filter(image_raw)
-            cv2.imshow("image_raw", img)
-            key = cv2.waitKey(1) #1ms待機=ほぼリアルタイムで表示できるはず
-            if key == 27:
-                break
+            nextimg = img
+            if not previmg is None:
+                diff = moving_object_extractor(previmg, nextimg)
+                output = dilation_image(20,3,diff)
+                cv2.imshow("image_raw", output)
+                key = cv2.waitKey(1) #1ms待機=ほぼリアルタイムで表示できるはず
+                if key == 27:
+                    break
         rate.sleep
     cv2.destroyAllWindows()
 
